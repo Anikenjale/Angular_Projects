3913fe589067a76edf8d6ae62b0cd4d24428eefd
Anikenjale <86092186+Anikenjale@users.noreply.github.com>
2024-01-15 23:39:19 +0530

    Add files via upload

---
+++ Assignment 7/srcQ1/app/second/second.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  standalone: true,
+  imports: [],
+  templateUrl: './second.component.html',
+  styleUrl: './second.component.css'
+})
+export class SecondComponent {
+
+  public text="Satara";
+
+  public text1="Educating for better Tomorrow";
+
+
+}
